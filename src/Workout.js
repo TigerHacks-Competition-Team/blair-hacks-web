a00a8edd706b80f37ccf3385ba3b4801615b2993
Gus Ruben <gusruben@protonmail.com>
2022-02-20 09:15:18 -0500

    Testing Chart

--- src/Workout.js
+++ src/Workout.js
@@ -3,11 +3,13 @@ import { withFirebase } from "./context";
 import HeartRate from "./HeartRate";
 import NavBar from "./NavBar";
 import "./App.css";
+import { LineChart, Line, XAxis, YAxis } from 'recharts';
 
 const Workout = (props) => {
   const [session, setSession] = useState(null);
   const [workingOut, setWorkingOut] = useState(false);
   const [bpm, setBpm] = useState(0);
+
 
   useEffect(() => {
     onBPMChange(bpm);
@@ -15,7 +17,12 @@ const Workout = (props) => {
 
   const onBPMChange = (bpm) => {
     console.log("bpm: ", bpm, "workingout", workingOut);
-    document.getElementById("bpm").innerText = Math.round(bpm);
+    // document.getElementById("bpm").innerText = Math.round(bpm);
+    // currentWorkout.shift();
+    // console.log(currentWorkout);
+    // currentWorkout[currentWorkout.length - 1] = {bpm: bpm};
+    // console.log(currentWorkout);
+
     if (workingOut) {
       setSession((prev) => {
         const updated = { ...prev };
@@ -66,6 +73,7 @@ const Workout = (props) => {
             <p>Workout</p>
           </div>
           <div id="start-button" onClick={() => {
+            setWorkingOut(true);
             document.getElementById("start-button").style.display = "none";
             document.getElementById("workout-ui").style.display = "initial";
           }}>
@@ -74,15 +82,22 @@ const Workout = (props) => {
           <div id="workout-ui">
             <div className="center">
               <div className="camera">
-                <HeartRate id="video-feed" onBPMChange={(bpm) => setBpm(bpm)} />
+                {workingOut && <HeartRate id="video-feed" onBPMChange={(bpm) => setBpm(bpm)} />}
                 {/* <div id="video-feed"></div> */}
               </div>
               <div className="bpm-feed">
-                <p id="bpm">82</p>
-                <p className="bpm-text">BPM</p>
+                {/* <p id="bpm">82</p>
+                <p className="bpm-text">BPM</p> */}
+                {session && (<LineChart width={640} height={480} data={session.heartRate}>
+                  <XAxis dataKey="date" />
+                  <YAxis/>
+                  <Line type="monotone" dataKey="bpm" stroke="#000000" />
+                  <p>heartrate: {JSON.stringify(session.heartRate)}</p>
+                </LineChart>)}
               </div>
             </div>
             <div id="stop-button" onClick={() => {
+              setWorkingOut(false);
               document.getElementById("start-button").style.display = "initial";
               document.getElementById("workout-ui").style.display = "none";
             }}>
